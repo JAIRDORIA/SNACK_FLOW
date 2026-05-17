1b691e62a3f5f625c4ebcfaecd8ec5d3fb0401cc
Dar <cantillolunad@gmail.com>
2026-05-16 20:03:18 -0500

    feat: agregar interceptor JWT en axios

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,10 +1,12 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://10.2.124.171:4000",  // URL de tu backend
+    baseURL: "http://192.168.1.2:4000",  // URL de tu backend
     headers: {
         "Content-Type": "application/json"
     }
 });
 
 export default api;
+
+

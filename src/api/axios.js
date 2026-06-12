69cc4af3335fa866102c93d9f4ed2721d2faa588
Dar <cantillolunad@gmail.com>
2026-06-12 16:54:58 -0500

    Url

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,10 +1,10 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://10.2.129.30:4000",  // URL de tu backend
+    baseURL: "http://192.168.1.8:4000",  // URL de tu backend
     headers: {
         "Content-Type": "application/json"
-    }
+    } 
 });
 
 // Agrega el token automáticamente en cada petición

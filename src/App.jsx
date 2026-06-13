34e667a142d757cbd87a1bdfe3f756c352f8c240
Jair Doria <jairdoria2020@gmail.com>
2026-06-13 14:38:49 -0500

    agregando primer corte

--- src/App.jsx
+++ src/App.jsx
@@ -11,8 +11,9 @@ import Balance from './pages/balance/Balance'
 import Compras from '@/pages/compras/Compras'
 import Proveedores from '@/pages/proveedores/proveedores'
 import Abonos from './pages/abonos/Abonos'
-import CombosManager    from './components/combosmanager'
-
+import CombosManager from './components/combosmanager'
+import PrimerCorte from './pages/pcorte/PrimerCorte'
+import RequireCorte from './components/RequireCortes'
 
 
 function RutaProtegida({ children }) {
@@ -25,20 +26,38 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/login" element={<Login />} />
+        <Route
+          path="/primer-corte"
+          element={
+            <RutaProtegida>
+              <PrimerCorte />
+            </RutaProtegida>
+          }
+        />
 
-        <Route path="/" element={<RutaProtegida><Layout /></RutaProtegida>}>
+        {/* Ruta principal: token + corte existente */}
+        <Route
+          path="/"
+          element={
+            <RutaProtegida>
+              <RequireCorte>
+                <Layout />
+              </RequireCorte>
+            </RutaProtegida>
+          }
+        >
           <Route index element={<Dashboard />} />
-          <Route path="ventas"               element={<Ventas />} />
-          <Route path="clientes"             element={<CustomersManager />} />
+          <Route path="ventas" element={<Ventas />} />
+          <Route path="clientes" element={<CustomersManager />} />
           <Route path="inventario/productos" element={<ProductsManager />} />
-          <Route path="inventario/ver"       element={<Inventario />} />
-          <Route path="inventario/combos"    element={<CombosManager />} />
-          <Route path="compras"              element={<Compras />} />
-          <Route path="balance"              element={<Balance />} />
-          <Route path="cortes"               element={<Cortes />} />
-          <Route path="abonos"               element={<Abonos />} />
-          <Route path="proveedores"          element={<Proveedores />} />
-          
+          <Route path="inventario/ver" element={<Inventario />} />
+          <Route path="inventario/combos" element={<CombosManager />} />
+          <Route path="compras" element={<Compras />} />
+          <Route path="balance" element={<Balance />} />
+          <Route path="cortes" element={<Cortes />} />
+          <Route path="abonos" element={<Abonos />} />
+          <Route path="proveedores" element={<Proveedores />} />
+
         </Route>
 
         <Route path="*" element={<Navigate to="/login" replace />} />

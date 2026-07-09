b97bab09d4f1f4b3174f54d22913a6165ee60ff7
Jair Doria <jairdoria2020@gmail.com>
2026-07-09 12:25:06 -0500

    solucionando editar venta

--- src/api/clientes_api.js
+++ src/api/clientes_api.js
@@ -3,6 +3,6 @@ import api from "./axios";
 export const getClientesTop = (limite = 5) =>
     api.get(`/clientes/top?limite=${limite}`);
 
-export const getClientes = (pagina = 1, limite = 20) =>
+export const getClientes = (pagina = 1, limite = 200) =>
     api.get(`/clientes/?pagina=${pagina}&limite=${limite}`);
 
